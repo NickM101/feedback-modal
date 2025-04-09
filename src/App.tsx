f237809cc75fd12c04e4d32e3f96a627020a72ab
NickM <nickmunene101@gmail.com>
2025-04-09 11:39:41 +0300

    Fixed dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -30,20 +30,20 @@ function App() {
   return (
     <div className="flex-1 justify-center">
       <div className="p-8 text-center">
-        <h1 className="text-2xl font-bold text-indigo-800 mb-4">
+        <h1 className="text-3xl font-bold text-perfume-800 dark:text-perfume-200 mb-4">
           Welcome to Feedback Modal
         </h1>
-        <p className="text-gray-700 mb-4">
+        <p className="text-gray-700 dark:text-gray-300 mb-4">
           We&apos;d love to hear your feedback.
         </p>
         <button
           onClick={openModal}
-          className="bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-2 px-6 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          className="bg-perfume-600 hover:bg-perfume-500 text-white font-semibold py-2 px-6 rounded focus:outline-none focus:ring-2 focus:ring-perfume-500"
         >
           Give Feedback
         </button>
         {feedbackRating !== null && (
-          <p className="mt-4 text-sm text-gray-600">
+          <p className="mt-4 text-sm text-gray-600 dark:text-gray-200">
             Your previous feedback rating:{" "}
             <span className="font-semibold">{feedbackRating}/10</span>
           </p>
